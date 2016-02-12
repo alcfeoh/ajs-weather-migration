7bd62fcd9ca790f74c95780567e01cfebaa13518
Alain Chautard <achautard@gmail.com>
2016-02-12 11:25:26 -0800

    After video 3.4

---
+++ app/scripts/services/locationsservice.ts
@@ -0,0 +1,20 @@
+export class LocationsService {
+
+    public locations:Array = [];
+
+    addLocation(location) {
+        this.locations.push(location);
+    }
+
+    getLocations() {
+        return this.locations;
+    }
+
+    removeLocation(zipcode: string) {
+        for (var i in this.locations) {
+            var value = this.locations[i];
+            if (value.zip === zipcode)
+                this.locations.splice(i, 1);
+        }
+    }
+}
