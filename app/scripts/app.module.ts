72ca0938ce863d1bfa7597dae995f31a2ad93fbe
Alain Chautard <achautard@gmail.com>
2016-02-24 17:43:19 -0800

    After video 4.4 - Angular 2 app completed

--- app/scripts/app.module.ts
+++ app/scripts/app.module.ts
@@ -6,45 +6,29 @@ import {WeatherService} from './services/weatherservice';
 import {currentConditions} from './components/currentconditions';
 import {Forecast} from './components/forecast';
 import {addLocation} from './components/addLocation';
-import {UpgradeAdapter} from 'angular2/upgrade';
 import {ForecastList} from "./components/forecastList";
 import {MainPage} from "./components/mainPage";
+import {RouteConfig, LocationStrategy, HashLocationStrategy, ROUTER_DIRECTIVES,ROUTER_PROVIDERS} from "angular2/router";
+import {bootstrap} from 'angular2/platform/browser';
+import {Component, provide} from "angular2/core";
 
+@RouteConfig([
+    {path:'/', component: MainPage},
+    {path:'/forecast', component: ForecastList}
+])
+@Component({
+    selector: 'weather-app',
+    template: '<router-outlet></router-outlet>',
+    directives: [ROUTER_DIRECTIVES]
+})
+class AppComponent {
 
-const upgradeAdapter = new UpgradeAdapter();
+}
 
-upgradeAdapter.addProvider(WeatherService);
-upgradeAdapter.addProvider(HTTP_PROVIDERS);
-
-angular
-    .module('weatherApp', [
-        'ngAnimate',
-        'ngResource',
-        'ngRoute',
-        'ngSanitize',
-        'ngTouch'
-    ])
-    .config(configure)
-    .service('WeatherService', upgradeAdapter.downgradeNg2Provider(WeatherService))
-    .directive('currentConditions', upgradeAdapter.downgradeNg2Component(currentConditions))
-    .directive('forecast', upgradeAdapter.downgradeNg2Component(Forecast))
-    .directive('forecasts', upgradeAdapter.downgradeNg2Component(ForecastList))
-    .directive('addLocation', upgradeAdapter.downgradeNg2Component(addLocation))
-    .directive('mainPage', upgradeAdapter.downgradeNg2Component(MainPage));
-
-configure.$inject = ['$routeProvider'];
-
-function configure($routeProvider) {
-    $routeProvider
-        .when('/', {
-            template: '<main-page></main-page>'
-        })
-        .when('/forecast', {
-             template: '<forecasts></forecasts>'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-};
-
-upgradeAdapter.bootstrap(document.documentElement, ['weatherApp']);
+bootstrap(AppComponent, [
+    HTTP_PROVIDERS,
+    ROUTER_PROVIDERS,
+    ROUTER_DIRECTIVES,
+    provide(LocationStrategy, {useClass: HashLocationStrategy}),
+    WeatherService
+]);
